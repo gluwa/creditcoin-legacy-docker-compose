cbaec362a9a13eef2941c70b374e64763b98e07b
Glenn <glenn.chin@gluwa.com>
2020-10-09 17:15:43 -0700

    WIP: Support for BitTorrent download on macOS

--- Server/upgrade/upgrade_creditcoin_node.sh
+++ Server/upgrade/upgrade_creditcoin_node.sh
@@ -4,17 +4,53 @@
 echo CREDITCOIN_HOME is $CREDITCOIN_HOME
 cd $CREDITCOIN_HOME  ||  exit 1
 
-[ -x "$(command -v transmission-cli)" ]  ||  {
-  echo 'transmission-cli' not found.
-  read -p "Install? (y/n) " yn
-  case $yn in
-    [Yy]*) apt-get update                       ||  exit 1
-           apt-get install -y transmission-cli  ||  exit 1    # BitTorrent client
-           ;;
-    *) exit 1
-       ;;
-  esac
+
+function install_torrent_client_on_ubuntu {
+  [ -x "$(command -v transmission-cli)" ]  ||  {
+    echo 'transmission-cli' not found.
+    read -p "Install? (y/n) " yn
+    case $yn in
+      [Yy]*) apt-get update                       ||  return 1
+             apt-get install -y transmission-cli  ||  return 1    # BitTorrent client
+             ;;
+      *) return 1
+         ;;
+    esac
+  }
+  return 0
 }
+
+
+function install_torrent_client_on_macos {
+  [ -x "$(command -v transmission-daemon)" ]  ||  {
+    echo 'transmission-daemon' not found.
+    read -p "Install? (y/n) " yn
+    case $yn in
+      [Yy]*) brew install transmission  ||  return 1
+             ;;
+      *) return 1
+         ;;
+    esac
+  }
+  return 0
+}
+
+
+os_name="$(uname -s)"
+case "${os_name}" in
+    Linux*)
+      install_torrent_client_on_ubuntu  ||  exit 1
+      TRANSMISSION_CLIENT=transmission-cli
+      ;;
+    Darwin*)
+      install_torrent_client_on_macos  ||  exit 1
+      lsof -i :51413 >/dev/null  ||  transmission-daemon -c .  ||  exit 1
+      TRANSMISSION_CLIENT="transmission-remote -a"
+      ;;
+    *) echo "Unsupported operating system: $os_name"
+       exit 1
+       ;;
+esac
 
 [ -z "$MAGNET_LINK" ]  &&  {
   MAGNET_LINK="magnet:?xt=urn:btih:5b2f2ebf6be7e37e7bdf71c5edf356a7dec87ca2&dn=creditcoin-block-volume.tar.gz&tr=udp%3a%2f%2ftracker.openbittorrent.com%3a80&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce"
@@ -33,7 +69,7 @@ do
   }  ||  echo $user_entered not found.
 done
 
-transmission-cli $MAGNET_LINK -w /  ||  exit 1
+$TRANSMISSION_CLIENT $MAGNET_LINK -w /  ||  exit 1
 
 BLOCK_VOLUME_FILE=`echo $MAGNET_LINK | sed -e 's/.*dn=\(.*\)\.tar\.gz.*/\1\.tar\.gz/'`
 BLOCK_VOLUME_PATH=/var/lib/docker/volumes/validator_validator-block-volume
@@ -43,10 +79,14 @@ BLOCK_VOLUME_PATH=/var/lib/docker/volumes/validator_validator-block-volume
 docker-compose -f $docker_compose down
 
 rm $BLOCK_VOLUME_PATH/_data/* 2>/dev/null
+mkdir -p $BLOCK_VOLUME_PATH  ||  exit 1
 tar xzvf /$BLOCK_VOLUME_FILE --directory $BLOCK_VOLUME_PATH
 
-docker-compose -f $docker_compose pull
-docker-compose -f $docker_compose build
-docker-compose -f $docker_compose up -d
+docker-compose -f $docker_compose pull             || exit 1
+docker-compose -f $docker_compose build >/dev/null || exit 1
+docker-compose -f $docker_compose up -d  &&  {
+  validator=`ps -ef | grep "[u]sr/bin/sawtooth-validator"`
+  [[ -z $validator ]]  &&  echo "Validator failed to start."  &&  exit 1
+}  ||  exit 1
 
 exit 0
